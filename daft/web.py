4bb4e21d0e52f269f3d2ce009e4bb759de8ce65c
Andrés Ambrois <andres@ambrois.org>
2019-12-26 23:11:51 +0000

    Add dynamic javascript tables.

--- daft/web.py
+++ daft/web.py
@@ -1,5 +1,6 @@
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
-from flask import Flask, g, render_template
+from flask import Flask, g, render_template, send_from_directory
 import sqlite3
 
 import daft_types
@@ -7,7 +8,8 @@ import daft_types
 def get_db():
     db = getattr(g, '_database', None)
     if db is None:
-        db = g._database = daft_types.get_db_connection()
+        db = g._database = daft_types.get_db_connection(
+                for_type=daft_types.Listing)
     return db
 
 app = Flask(__name__)
@@ -20,9 +22,9 @@ def close_connection(exception):
 
 @app.route('/')
 def hello_world():
-    cur = get_db().execute('SELECT * from listings;')
-    return render_template('list.html', listings=cur.fetchall())
+    cur = get_db().execute('SELECT * from listing;')
+    return render_template('index.html', listings=cur.fetchall())
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
